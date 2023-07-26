a25f22fe372727ef7b702881bab2a6ea3cf3ebbe
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2023-07-26 16:47:56 +0300

    Displayed genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const GameCardContainer: React.FC<Props> = ({ children }) => {
   return (
-    <Box borderRadius={10} overflow="hidden" width="300px">
+    <Box borderRadius={10} overflow="hidden" width="100%">
       {children}
     </Box>
   );
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,7 +15,7 @@ const GameGrid: React.FC = () => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
+        spacing={3}
         padding="10px">
         {isLoading &&
           skeletons.map((skeleton) => (
