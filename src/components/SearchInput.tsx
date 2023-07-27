9f8f69726ac349468696b6b1fffb2f26b87118b9
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2023-07-27 20:35:44 +0300

    Fixed navbar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,16 +3,21 @@ import { IoSearch } from "react-icons/io5";
 import { useRef } from "react";
 
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (ref.current) setSearchText(ref.current.value);
+        if (ref.current) {
+          setSearchText(ref.current.value);
+          navigate("/");
+        }
       }}>
       <InputGroup>
         <InputLeftElement children={<IoSearch />} />
